405de7b600aea36b2379315d92c5e762ed4acb75
Andrei Iulian Luca <luca.andrei@info.uaic.ro>
2021-09-22 15:53:27 +0300

    bug search bar - to be fixed

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -1,8 +1,10 @@
-const add = document.querySelector(".add");
-const list = document.querySelector(".todos");
+const add = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const searchBar = document.querySelector('.search input');
+
 const generateToDo = (todo) =>{
     const html = `
-    <li class="list-group-item d-flex justify-content-between align-items-center mb-1">
+    <li class="list-group-item d-flex justify-content-between align-items-center mb-1 text-light">
         <span>${todo}</span>
         <i class="far fa-trash-alt delete"></i>
     </li>
@@ -19,4 +21,28 @@ add.addEventListener('submit', e =>{
         generateToDo(contain);
         add.reset();
 }
-})
+});
+
+list.addEventListener('click', e =>{
+
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+const filterToDo = term => {
+
+    Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+
+};
+
+searchBar.addEventListener('keyup', () =>{
+    const term = searchBar.value.trim().toLowerCase();
+    filterToDo(term);
+});
